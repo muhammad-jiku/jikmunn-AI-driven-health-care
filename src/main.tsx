56f18326b7d40293b19befff6a3d9e77db80d2cd
muhammad-jiku <muhammadjiku364@gmail.com>
2025-03-27 16:46:45 +0600

    privy auth setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { PrivyProvider } from '@privy-io/react-auth';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App.tsx';
+import './index.css';
+
+console.log(
+  import.meta.env.VITE_PRIVY_APP_ID,
+  import.meta.env.VITE_PRIVY_CLIENT_ID
+);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <PrivyProvider
+      appId={`${import.meta.env.VITE_PRIVY_APP_ID}`}
+      clientId={`${import.meta.env.VITE_PRIVY_CLIENT_ID}`}
+      config={{
+        // // Display email and wallet as login methods
+        // loginMethods: ['email', 'wallet'],
+        appearance: {
+          theme: 'dark',
+          // accentColor: '#676FFF',
+          // logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        // embeddedWallets: {
+        //   createOnLogin: 'users-without-wallets',
+        // },
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PrivyProvider>
+    ,
+  </StrictMode>
+);
